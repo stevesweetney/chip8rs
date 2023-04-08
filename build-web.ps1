548bf505fa94e03c6572bd11203fca3c5c66dd87
Steve Sweetney <stevesweetney@gmail.com>
2023-04-07 23:40:46 -0400

    Add powershell script to build for the wasm target - related https://gist.github.com/tgolsson/d78f7887a8542f3fd6f125070e5e22d6

---
+++ build-web.ps1
@@ -0,0 +1,20 @@
+# Set ErrorActionPreference to Stop to stop the script if there is an error
+$ErrorActionPreference = "Stop"
+
+# Define variables
+$target = "wasm32-unknown-unknown"
+$outDir = "dist"
+
+# Build the Rust project
+cargo build --target $target --profile release-small
+
+# Generate bindgen outputs
+mkdir -Force $outDir
+wasm-bindgen --target web --out-dir $outDir/ target/$target/release-small/chip8-frontend.wasm --no-typescript
+
+# Shim to tie the thing together
+$content = (Get-Content "$outDir/chip8-frontend.js") -replace "import \* as __wbg_star0 from 'env';", "" `
+    -replace "let wasm;", "let wasm; export const set_wasm = (w) => wasm = w;" `
+    -replace "imports\['env'\] = __wbg_star0;", "return imports.wbg;"
+$content = $content.Replace("const imports = getImports();", "return getImports();")
+$content | Set-Content "$outDir/chip8-frontend.js"
