eebd2253321260373967bb46631ce11469b7ca81
Steve Sweetney <stevesweetney@gmail.com>
2023-04-09 19:46:18 -0400

    Update input mapping - use layout suggested in https://multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/

--- chip8-frontend/src/input_mapping.rs
+++ chip8-frontend/src/input_mapping.rs
@@ -1,22 +1,22 @@
 use macroquad::input::KeyCode;
 
 pub const ACCEPTED_KEYS: [KeyCode; 16] = [
-    KeyCode::Key0,
     KeyCode::Key1,
     KeyCode::Key2,
     KeyCode::Key3,
     KeyCode::Key4,
-    KeyCode::Key5,
-    KeyCode::Key6,
-    KeyCode::Key7,
-    KeyCode::Key8,
-    KeyCode::Key9,
-    KeyCode::A,
-    KeyCode::B,
-    KeyCode::C,
-    KeyCode::D,
+    KeyCode::Q,
+    KeyCode::W,
     KeyCode::E,
+    KeyCode::R,
+    KeyCode::A,
+    KeyCode::S,
+    KeyCode::D,
     KeyCode::F,
+    KeyCode::Z,
+    KeyCode::X,
+    KeyCode::C,
+    KeyCode::V,
 ];
 pub struct KeyValue(pub u8);
 
@@ -25,22 +25,22 @@ impl TryFrom<KeyCode> for KeyValue {
 
     fn try_from(keycode: KeyCode) -> Result<Self, Self::Error> {
         match keycode {
-            KeyCode::Key0 => Ok(KeyValue(0)),
-            KeyCode::Key1 => Ok(KeyValue(1)),
-            KeyCode::Key2 => Ok(KeyValue(2)),
-            KeyCode::Key3 => Ok(KeyValue(3)),
-            KeyCode::Key4 => Ok(KeyValue(4)),
-            KeyCode::Key5 => Ok(KeyValue(5)),
-            KeyCode::Key6 => Ok(KeyValue(6)),
-            KeyCode::Key7 => Ok(KeyValue(7)),
-            KeyCode::Key8 => Ok(KeyValue(8)),
-            KeyCode::Key9 => Ok(KeyValue(9)),
-            KeyCode::A => Ok(KeyValue(10)),
-            KeyCode::B => Ok(KeyValue(11)),
-            KeyCode::C => Ok(KeyValue(12)),
-            KeyCode::D => Ok(KeyValue(13)),
-            KeyCode::E => Ok(KeyValue(14)),
-            KeyCode::F => Ok(KeyValue(15)),
+            KeyCode::Key1 => Ok(KeyValue(0x1)), // 1 => 1
+            KeyCode::Key2 => Ok(KeyValue(0x2)), // 2 => 2
+            KeyCode::Key3 => Ok(KeyValue(0x3)), // 3 => 3
+            KeyCode::Key4 => Ok(KeyValue(0xC)), // 4 => C
+            KeyCode::Q => Ok(KeyValue(0x4)),    // Q => 4
+            KeyCode::W => Ok(KeyValue(0x5)),    // W => 5
+            KeyCode::E => Ok(KeyValue(0x6)),    // E => 6
+            KeyCode::R => Ok(KeyValue(0xD)),    // R => D
+            KeyCode::A => Ok(KeyValue(0x7)),    // A => 7
+            KeyCode::S => Ok(KeyValue(0x8)),    // S => 8
+            KeyCode::D => Ok(KeyValue(0x9)),    // D => 9
+            KeyCode::F => Ok(KeyValue(0xE)),    // F => E
+            KeyCode::Z => Ok(KeyValue(0xA)),    // Z => A
+            KeyCode::X => Ok(KeyValue(0x0)),    // X => 0
+            KeyCode::C => Ok(KeyValue(0xB)),    // C => B
+            KeyCode::V => Ok(KeyValue(0xF)),    // V => F
             _ => Err(format!("Unimplemented keycode {:?}", keycode)),
         }
     }
